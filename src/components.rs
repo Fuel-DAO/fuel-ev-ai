66cb14cb236f77c959fde28e67f7cf1ca1d0db47
veercodeprog <veerpratap945050@gmail.com>
2024-10-30 14:12:29 +0530

    collection page ui

--- src/components.rs
+++ src/components.rs
@@ -1,8 +1,10 @@
 pub mod account;
+pub mod footer;
 pub mod header;
+pub mod header2;
 pub mod home_banner;
+pub mod powered_by;
 pub mod ride_sharing;
 pub mod ridesharing_info;
 pub mod trip_includes;
-pub mod powered_by;
-pub mod footer;
+
