20fdade87808dd5e5b407d21cc709f2ee252bb9a
veer-pratap-fueldao <veer@fueldao.io>
2024-11-05 02:24:11 +0530

    update

--- src/components/header.rs
+++ src/components/header.rs
@@ -79,16 +79,16 @@ fn UserPrincipal() -> impl IntoView {
                     >
                         <svg
                             xmlns="http://www.w3.org/2000/svg"
-                            class="h-6 w-6"
                             fill="none"
                             viewBox="0 0 24 24"
+                            stroke-width="1.5"
                             stroke="currentColor"
-                            stroke-width="2"
+                            class=""
                         >
                             <path
                                 stroke-linecap="round"
                                 stroke-linejoin="round"
-                                d="M12 14l9-5-9-5-9 5 9 5zm0 7l9-5-9-5-9 5 9 5z"
+                                d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z"
                             />
                         </svg>
                     </button>
